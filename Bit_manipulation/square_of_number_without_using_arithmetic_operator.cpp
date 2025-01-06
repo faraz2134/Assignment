46d187e3ba886f6b543c1cdcaabc29ad64ffdf55
faraz2134 <cse23201@glbitm.ac.in>
2025-01-06 17:27:53 +0530

    Create square_of_number_without_using_arithmetic_operator.cpp

---
+++ Bit_manipulation/square_of_number_without_using_arithmetic_operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int square(int i) {
+        if (i<0)i= -i; 
+        int result = 0, temp = i, shift = 0;
+       while (temp > 0) {
+            if (temp & 1) { 
+                result += (i<< shift); }
+            shift++;       
+            temp >>= 1; }
+        return result;
+    }};
+
