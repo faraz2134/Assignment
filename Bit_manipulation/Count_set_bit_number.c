7abca268e3ec3426b2f8e9593908bffe4efbaa71
faraz2134 <cse23201@glbitm.ac.in>
2025-01-06 14:11:26 +0530

    Create Count_set_bit_number.c

---
+++ Bit_manipulation/Count_set_bit_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void countBits(int n, int res[]) {
+    for (int j = 0; j <= n; j++) {
+        res[j] = res[j >> 1] + (j & 1)}
+}
+int main() {
+    int n = 5;
+    int res[n + 1]; 
+    
+    countBits(n, res);
+
+    printf("Result: ");
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", res[i]);
+    }
+    printf("\n");
+   return 0;}
