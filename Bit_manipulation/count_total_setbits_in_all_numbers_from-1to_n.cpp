a839459675057016b6f924030ef46a66f20077fa
faraz2134 <cse23201@glbitm.ac.in>
2025-01-06 17:24:04 +0530

    Create count_total_setbits_in_all_numbers_from-1to_n.cpp

---
+++ Bit_manipulation/count_total_setbits_in_all_numbers_from-1to_n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int countTotalSetBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) { 
+            int Mask = 1 << i;
+            result += ((n + 1) / (2 * Mask)) * Mask +
+                            max(0, (n + 1) % (2 * Mask) - Mask);
+        }
+        return result;
+    }
+};
