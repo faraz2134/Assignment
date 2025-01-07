4575323eb40a781df49e2c5d04ce95321601ed11
faraz2134 <cse23201@glbitm.ac.in>
2025-01-07 10:50:34 +0530

    Update Count_set_bit_number.c

--- Bit_manipulation/Count_set_bit_number.c
+++ Bit_manipulation/Count_set_bit_number.c
@@ -2,7 +2,7 @@
 
 void countBits(int n, int res[]) {
     for (int j = 0; j <= n; j++) {
-        res[j] = res[j >> 1] + (j & 1)}
+        res[j] = res[j >> 1] + (j & 1);}
 }
 int main() {
     int n = 5;
